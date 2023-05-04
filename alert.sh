d6aafb1ce506f0fa3aa7337a3be154b601e1d175
Ruben Oostinga <ruben.oostinga@gmail.com>
2023-05-04 10:05:29 +0200

    Update for wayland

--- alert.sh
+++ alert.sh
@@ -3,9 +3,11 @@ set -o pipefail
 set -o nounset
 set -o errexit
 
-dimensions=$(xdpyinfo | awk '/dimensions/{print $2}')
-w=$(echo "$dimensions" | cut -f1 -dx); \
-h=$(echo "$dimensions" | cut -f2 -dx); \
+#dimensions=$(xdpyinfo | awk '/dimensions/{print $2}')
+#w=$(echo "$dimensions" | cut -f1 -dx); \
+#h=$(echo "$dimensions" | cut -f2 -dx); \
+w=2000
+h=2000
 zenity --error \
 --text "<span size=\"xx-large\" weight=\"bold\">$1</span>" \
 --width="$w" --height="$h" &
